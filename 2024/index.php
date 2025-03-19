dfd6355edc201c9e6e152cb56819680b3731fab6
coinfsei <coinf@sei.ba.gov.br>
2025-03-19 20:01:33 -0300

    Refactor: ajusta estilos e estrutura de páginas, remove seção de submissão

--- 2024/index.php
+++ 2024/index.php
@@ -2,6 +2,8 @@
 <html lang="pt-br">
 
 <head>
+	<!-- Head -->
+    <?php include('./snippets/head.html') ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>XII FBEA - XII Fórum Baiano de Economia Aplicada</title>
@@ -10,17 +12,9 @@
     <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css" rel="stylesheet">
-	
-	<!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-TVLD0M8TF7"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-
-		gtag('config', 'G-TVLD0M8TF7');
-	</script>
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Sofia+Sans+Condensed:ital,wght@0,1..1000;1,1..1000&display=swap" rel="stylesheet">
 
 </head>
 
@@ -36,16 +30,17 @@
     <?php include('./snippets/sobre.html'); ?>
 
     <!-- Sessão Submissão -->
-    <?php //include('./snippets/submissao.html'); ?>
+    <?php //include('./snippets/submissao.html'); 
+    ?>
 
     <!-- Sessão de Programação -->
     <?php include('./snippets/programacao.html'); ?>
-    
-     <!-- Modal de Download -->
-     <!-- include('./snippets/download.html'); -->
+
+    <!-- Modal de Download -->
+    <?php include('./snippets/download.html'); ?>
 
     <!-- Seção Palestrantes -->
-    <!-- include('./snippets/palestrantes.html'); -->
+    <?php include('./snippets/palestrantes.html'); ?>
 
     <!-- Sessão dos Patrocinadores -->
     <?php include('./snippets/patrocinio.html'); ?>
@@ -57,10 +52,7 @@
     <?php include('./snippets/rodape.html'); ?>
 
     <!-- Chamada dos Scripts -->
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="script.js"></script>
+    <?php include('./snippets/scripts.html') ?>
 </body>
 
 </html>
